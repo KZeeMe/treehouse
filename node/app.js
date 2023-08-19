e877b28826f67b01d5817a3f2b478eb9d87a6f8a
KZeeMe <102718492+KZeeMe@users.noreply.github.com>
2023-08-19 10:20:45 -0500

    added node app error handling

--- node/app.js
+++ node/app.js
@@ -1,4 +1,5 @@
 const https = require("https");
+const http = require("http");
 
 // Print the data
 function printMessage(username, badgeCount, points) {
@@ -6,33 +7,55 @@ function printMessage(username, badgeCount, points) {
   console.log(message);
 }
 
-function getProfile(username) {
-  // connect to API URL (https://teamtreehouse.com/kzaloudek.json)
-  const request = https.get(
-    `https://teamtreehouse.com/profiles/${username}.json`,
-    (response) => {
-      let body = "";
-      console.dir(response.statusCode);
-
-      // Read the data
-      response.on("data", (data) => {
-        body += data.toString();
-      });
-      response.on("end", () => {
-        // Parse the data
-        let profile = JSON.parse(body);
-
-        // console.dir(JSON.parse(body));
-
-        printMessage(
-          username,
-          profile.badges.length,
-          profile.points.JavaScript
-        );
-      });
-    }
-  );
-  request.on('error',error => console.error(error));
+function printError(error) {
+  console.error(error.message);
 }
 
-getProfile("kristazaloudek");
+function getProfile(username) {
+  try {
+    // connect to API URL (https://teamtreehouse.com/kzaloudek.json)
+    const request = https.get(
+      `https://teamtreehouse.com/profiles/${username}.json`,
+      (response) => {
+        if (response.statusCode === 200) {
+          let body = "";
+          //console.dir(response.statusCode);
+
+          // Read the data
+          response.on("data", (data) => {
+            body += data.toString();
+          });
+          response.on("end", () => {
+            try {
+              // Parse the data
+              let profile = JSON.parse(body);
+
+              // console.dir(JSON.parse(body));
+
+              printMessage(
+                username,
+                profile.badges.length,
+                profile.points.JavaScript
+              );
+            } catch (error) {
+              printError(error);
+            }
+          });
+        } else {
+          const message = `There was an error getting the profile for ${username} (${
+            http.STATUS_CODES[response.statusCode]
+          })`;
+          const statusCodeError = new Error(message);
+          printError(statusCodeError);
+        }
+      }
+    );
+
+    request.on("error", (error) => console.error(error.code));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const users = process.argv.slice(2);
+users.forEach(getProfile);
