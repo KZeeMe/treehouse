5dee54a410fac4a4ffe812da8edb0360583f4ef1
KZeeMe <102718492+KZeeMe@users.noreply.github.com>
2023-08-23 18:14:53 -0500

    edit widget file

--- ruby/intro/widgets.rb
+++ ruby/intro/widgets.rb
@@ -3,7 +3,12 @@ def ask(question)
     gets.chomp
 end
 
+def price(quantity)
+    quantity * 10
+end
+
 puts "Welcome to the widget store!"
 answer = ask("How many widgets are you ordering?")
 number = answer.to_i
+total = price(number)
 puts "For #{answer} widgets, your total is $#{ number * 10 }"
